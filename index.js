e02e2ad1aa717bfdd88c12a3cc21bc2108c49e96
envioues123 <chaemwarisa@gmail.com>
2025-05-19 15:27:42 +0700

    index.js

--- index.js
+++ index.js
@@ -2,12 +2,16 @@ const puppeteer = require('puppeteer');
 
 (async () => {
   const browser = await puppeteer.launch({
-    headless: 'new', // ใช้ 'true' หาก headless ธรรมดาไม่ได้
+    executablePath: '/usr/bin/google-chrome',
+    args: ['--no-sandbox', '--disable-setuid-sandbox'],
+    headless: true
   });
+
   const page = await browser.newPage();
   await page.goto('https://envioues123.github.io/baccarat-template/', {
     waitUntil: 'networkidle2'
   });
+
   await page.screenshot({ path: 'screenshot.png', fullPage: true });
   await browser.close();
 })();
